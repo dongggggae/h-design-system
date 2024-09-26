0160eff5f23de95a3f66add0a6359bc64d218b98
김동현 <doyn0822@hiconsy.com>
2024-09-26 16:43:47 +0900

    [feat]뱃지 컴포넌트 추가

---
+++ src/components/Badge/index.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import classNames from '@utils/classNames';
+
+import BadgeText from './Text';
+import '@styles/hds.scss';
+
+interface BadgeProps {
+  text: string;
+  size?: 'md' | 'sm';
+  state?: 'success' | 'warnning' | 'error' | 'information';
+  outline?: boolean;
+  className?: string | string[];
+}
+
+const Badge: React.FC<BadgeProps> = ({ text, size = 'sm', state = 'success', outline = false, className }) => {
+  const PREFIX = 'badge';
+
+  return (
+    <span
+      className={classNames(
+        PREFIX,
+        size && `${PREFIX}--${size}`,
+        state && `${PREFIX}--${state}`,
+        outline ? `${PREFIX}__outline--${state}` : '',
+        Array.isArray(className) ? className.join(' ') : className,
+      )}
+    >
+      <BadgeText text={text} />
+    </span>
+  );
+};
+
+Badge.displayName = 'Badge';
+
+export default Badge;
