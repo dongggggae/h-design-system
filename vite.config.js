7873d7c0e206a96dcac3da8f1a71f3dafa8ee05c
김동현 <doyn0822@hiconsy.com>
2024-09-26 16:38:59 +0900

    [chore]프로젝트 세팅 수정(tsconfig, vite.config, package.json)

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,42 @@
 import { defineConfig } from 'vite'
+import { fileURLToPath } from 'node:url'
+import dts from 'vite-plugin-dts';
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
-// https://vitejs.dev/config/
+const __dirname = fileURLToPath(new URL('.', import.meta.url))
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), dts()],
+  server: {
+    port: 8080,
+  },
+  resolve: {
+    alias: [
+      { find: "@components", replacement: path.resolve(__dirname, "src/components") },
+      { find: "@styles", replacement: path.resolve(__dirname, "src/styles") },
+      { find: "@utils", replacement: path.resolve(__dirname, "src/utils") },
+    ]
+  },
+  css: {
+    devSourcemap: true,
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: 'MyLibrary',
+      fileName: (format) => `index.${format}.js`,
+      formats: ['es', 'cjs'],
+    },
+    rollupOptions: {
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+  },
 })
