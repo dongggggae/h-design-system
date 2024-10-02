f56adc0513ae06f0f3fb200ced21beb424978e09
김동현 <doyn0822@hiconsy.com>
2024-10-02 14:57:51 +0900

    [DS-105]feat - icon 컴포넌트 추가

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 export { default as Badge } from '@components/Badge';
 export type { BadgeProps } from '@components/Badge';
 
-export {default as BadgeText} from '@components/Badge/Text'
+export { default as BadgeText } from '@components/Badge/Text';
 export type { BadgeTextProps } from '@components/Badge/Text';
 
 export { default as Heading } from '@components/Heading';
 export type { HeadingProps } from '@components/Heading';
+
+export { default as Icon } from '@components/Icon';
+export type { IconProps } from '@components/Icon';
 
 export { default as Text } from '@components/Text';
 export type { TextProps } from '@components/Text';
