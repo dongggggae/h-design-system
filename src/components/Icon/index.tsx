9c7ff2e0bcf2378eb83b7c78660f2b2cf107644f
김동현 <doyn0822@hiconsy.com>
2024-10-02 14:57:51 +0900

    [DS-105]feat - icon 컴포넌트 추가

---
+++ src/components/Icon/index.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import classNames from '@utils/classNames';
+
+export interface IconProps {
+  name: string;
+  size?: 'lg' | 'md' | 'sm';
+  color?: string;
+  className?: string | string[];
+}
+
+const Icon: React.FC<IconProps> = ({ name, size = 'md', color = 'white', className }) => {
+  const PREFIX = 'icon';
+
+  return (
+    <i
+      className={classNames(
+        PREFIX,
+        `${PREFIX}__${name}`,
+        `${PREFIX}--${size}`,
+        `${PREFIX}--${color}`,
+        Array.isArray(className) ? className.join(' ') : className,
+      )}
+    >
+      <b className="blind">{name}</b>
+    </i>
+  );
+};
+
+Icon.displayName = 'Icon';
+
+export default Icon;
