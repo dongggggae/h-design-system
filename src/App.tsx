f2b118732132b3cc939078d4e1aa7431b98840ba
김동현 <doyn0822@hiconsy.com>
2024-10-04 16:09:23 +0900

    [DS-106]feat - btn 컴포넌트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import Btn from '@components/Btn';
 
 const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Btn text="asd" icon="close" size="sm" />
+    </div>
+  );
 };
 
 export default App;
