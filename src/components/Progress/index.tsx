11b06679b95637a1e60f4be4ec8b6958d46b2191
김동현 <doyn0822@hiconsy.com>
2024-10-04 12:06:44 +0900

    [DS-113]feat - progress 컴포넌트 추가

---
+++ src/components/Progress/index.tsx
@@ -0,0 +1,32 @@
+import React, { useRef, useEffect } from 'react';
+
+import classNames from '@utils/classNames';
+
+interface ProgressProps {
+  now: number;
+  label?: boolean;
+  state?: 'success' | 'warnning' | 'error' | 'information';
+  className?: string | string[];
+}
+
+const Progress: React.FC<ProgressProps> = ({ now, label, state = 'success', className }) => {
+  const PREFIX = 'progress';
+
+  const progressBarRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (progressBarRef.current) {
+      progressBarRef.current.style.width = `${now}%`;
+    }
+  });
+
+  return (
+    <div className={classNames(PREFIX, Array.isArray(className) ? className.join(' ') : className)}>
+      <div ref={progressBarRef} data-persent={now} className={classNames(`${PREFIX}-bar`, state)}>
+        {label && `${now}%`}
+      </div>
+    </div>
+  );
+};
+
+export default Progress;
