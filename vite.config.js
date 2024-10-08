48aac9d6c4aba9d75a2b867aece18cae5f51ad4c
김동현 <doyn0822@hiconsy.com>
2024-10-08 16:36:29 +0900

    [DS-115]chore - 빌드시 sourcemap false로 변경

--- vite.config.js
+++ vite.config.js
@@ -23,7 +23,7 @@ export default defineConfig({
   },
   build: {
     outDir: 'dist',
-    sourcemap: true,
+    sourcemap: false,
     lib: {
       entry: path.resolve(__dirname, 'src/index.ts'),
       name: 'MyLibrary',
