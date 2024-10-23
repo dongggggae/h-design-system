939aee5ee26ed7cd6aa6deda9f7c79756265abd7
김동현 <doyn0822@hiconsy.com>
2024-10-23 15:53:55 +0900

    [DS-118]feat - FormsCheck 컴포넌트 추가

---
+++ src/components/Forms/FormsCheck.tsx
@@ -0,0 +1,41 @@
+import React, { InputHTMLAttributes, forwardRef } from 'react';
+
+import classNames from '@utils/classNames';
+
+import FormsLabel from './FormsLabel';
+
+export interface FormsCheckProps extends InputHTMLAttributes<HTMLInputElement> {
+  id: string;
+  type?: 'checkbox' | 'radio' | 'switch';
+  label?: string;
+  reverse?: boolean;
+  checked?: boolean;
+  className?: string | undefined;
+}
+
+const FormsCheck = forwardRef<HTMLInputElement, FormsCheckProps>(
+  ({ id, type, label, reverse, checked, className, ...props }, ref) => {
+    const PREFIX = 'form__check';
+    const isSwitch = type === 'switch';
+
+    return (
+      <div className={classNames(PREFIX, isSwitch ? 'form__switch' : '', reverse ? 'reverse' : '')}>
+        {reverse && label && <FormsLabel htmlFor={id}>{label}</FormsLabel>}
+        <input
+          id={id}
+          ref={ref}
+          type={isSwitch ? 'checkbox' : type}
+          role={type}
+          checked={checked}
+          className={classNames(`${PREFIX}-input`, className)}
+          {...props}
+        />
+        {!reverse && label && <FormsLabel htmlFor={id}>{label}</FormsLabel>}
+      </div>
+    );
+  },
+);
+
+FormsCheck.displayName = 'FormsCheck';
+
+export default FormsCheck;
